744b5c9e8c9807a03cc4a9b3cab0a761e38d43a6
devtescode <teslimagboola09@gmail.com>
2025-01-21 14:57:52 +0100

    updated transferform

--- src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
+++ src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
@@ -20,113 +20,151 @@ function TransactionReceipt() {
 
 
   return (
-    <div className="container py-4" style={{ maxWidth: "480px" }}>
-      {/* Header */}
-      <div className="d-flex align-items-center mb-4">
+    <>
+      <div className="d-flex align-items-center shadow-lg col-md-5 col-sm-12 mx-auto" >
         {/* <ArrowLeft className="me-2" size={24} /> */}
-        <h5 className="mb-0">Share Receipt</h5>
+        <h5 className="mb-0 d-flex text-muted" style={{ alignItems: "center" }}><i class="ri-arrow-drop-left-line text-muted" style={{ fontSize: "40px" }}></i> Share Receipt</h5>
       </div>
+      <div className="container mt-4" style={{ maxWidth: "580px" }}>
+        {/* Header */}
 
-      {/* Receipt Card */}
-      <div className="card">
-        <div className="card-body">
-          {/* Logo and Title */}
-          <div className="text-center mb-4 border">
-            <img
-              //   src={`${process.env.PUBLIC_URL}/opay-logo.png`} 
-              alt="OPay"
-              height="40"
-              className="mb-3"
-            />
-            <h5>Transaction Receipt</h5>
+        {/* Receipt Card */}
+        <div className="card">
+          <div className="card-body">
+            {/* Logo and Title */}
+            <div className="text-center mb-4 d-flex justify-content-between" style={{ alignItems: "center" }}>
+              <img
+                src="https://imgs.search.brave.com/hiRXT07PsWfuYI1jH8-WQAHkAkX7QrBODxnnzV4f6Go/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90ZWNo/cG9pbnQuYWZyaWNh/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIz/LzA3L25ldy1sb2dv/LTEwMjR4NTc2Lmpw/ZWcud2VicA"
+                alt="Image description"
+                height="60"
+                className=""
+              />
+
+              <h5 className='text-muted'>Transaction Receipt</h5>
+            </div>
+
+            {/* Amount and Status */}
+            <div className="text-center mb-4">
+              <h2 className="text-success"> {amount !== null ? `₦${amount.toLocaleString(undefined, { minimumFractionDigits: 2 })}` : 'Loading...'}</h2>
+              <h5 className="" style={{ fontWeight: "normal" }}>Successful</h5>
+              <small className="text-muted">Jan 20th, 2025 14:50:33</small>
+            </div>
+
+            {/* Transaction Details */}
+            <div className="mb-3">
+              <div className="row mb-2">
+                <div className="col-5">
+                  <span className="text-muted">Recipient Details</span>
+                </div>
+                <div className="col-7 text-end">
+                  {/* <span>{accountDetails.accountName} {accountDetails.bankName} | {accountDetails.accountNumber} </span> */}
+                  {accountDetails ? (
+                    <div>
+                      <p className="mb-1 text-muted" style={{ fontSize: "12px" }}>{accountDetails.accountName}</p>
+                      <p className="mb-0 text-muted">
+                        {accountDetails.bankName} | {accountDetails.accountNumber}
+                      </p>
+                    </div>
+                  ) : (
+                    <p>No account details found. Please go back and select an account.</p>
+                  )}
+                </div>
+              </div>
+
+              <div className="row mb-2">
+                <div className="col-5">
+                  <span className="text-muted">Sender Details</span>
+                </div>
+                <div className="col-7 text-end">
+                  <span className="text-muted">OPay | 806****821</span>
+                </div>
+              </div>
+
+              <div className="row mb-2">
+                <div className="col-5">
+                  <span className="text-muted">Transaction Type</span>
+                </div>
+                <div className="col-7 text-end">
+                  <span className="text-muted">Transfer to Bank Account</span>
+                </div>
+              </div>
+
+              <div className="row mb-2">
+                <div className="col-5">
+                  <span className="text-muted">Transaction No.</span>
+                </div>
+                <div className="col-7 text-end">
+                  <span className="text-break text-muted" >250120020100886992707982</span>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-5">
+                  <span className="text-muted">Session ID</span>
+                </div>
+                <div className="col-7 text-end">
+                  <span className="text-break text-muted">100004250120135044125751241279</span>
+                </div>
+              </div>
+            </div>
+
+            {/* Footer Text */}
+            <div className="text-center text-muted small " style={{ fontSize: "8px", marginTop: "50px" }}>
+              <p style={{ textAlign: "justify" }}>Enjoy a better life with OPay. Get free transfers, withdrawals, bill payments, instant loans, and good annual interest on your savings. OPay is licensed by the Central Bank of Nigeria and insured by the NDIC.</p>
+            </div>
+
+            {/* Share Buttons */}
+
           </div>
+        </div>
+        <div className="row g-2 mt-3 fixed-bottom shadow" style={{ alignItems: "center" }}>
+          <div className='d-flex justify-content-between p-4' style={{ alignItems: "center" }}>
 
-          {/* Amount and Status */}
-          <div className="text-center mb-4">
-            <h2 className="text-success"> {amount !== null ? `₦${amount.toLocaleString(undefined, { minimumFractionDigits: 2 })}` : 'Loading...'}</h2>
-            <h5 className="" style={{fontWeight:"normal"}}>Successful</h5>
-            <small className="text-muted">Jan 20th, 2025 14:50:33</small>
-          </div>
 
-          {/* Transaction Details */}
-          <div className="mb-3">
-            <div className="row mb-2">
-              <div className="col-5">
-                <span className="text-muted">Recipient Details</span>
-              </div>
-              <div className="col-7 text-end">
-                {/* <span>{accountDetails.accountName} {accountDetails.bankName} | {accountDetails.accountNumber} </span> */}
-                {accountDetails ? (
-                            <div>
-                                <p className="mb-1 text-muted" style={{fontSize:"12px"}}>{accountDetails.accountName}</p>
-                                <p className="mb-0 text-muted">
-                                    {accountDetails.bankName} | {accountDetails.accountNumber} 
-                                </p>
-                            </div>
-                        ) : (
-                            <p>No account details found. Please go back and select an account.</p>
-                        )}  
+            <div className=" text-success text-center" style={{ alignItems: "center" }}>
+
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className=''>
+                  <i class="ri-image-2-line fs-1 "></i>
+                </div>
+                <div>
+                  Share as image
+                </div>
               </div>
             </div>
 
-            <div className="row mb-2">
-              <div className="col-5">
-                <span className="text-muted">Sender Details</span>
-              </div>
-              <div className="col-7 text-end">
-                <span className="text-muted">OPay | 806****821</span>
+            <div className=" text-center" style={{ alignItems: "center" }}>
+
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className='text-muted'>
+                  |
+                </div>
+                {/* <div>
+                  |
+                </div> */}
               </div>
             </div>
 
-            <div className="row mb-2">
-              <div className="col-5">
-                <span className="text-muted">Transaction Type</span>
-              </div>
-              <div className="col-7 text-end">
-                <span className="text-muted">Transfer to Bank Account</span>
+            <div className=" text-success text-center" style={{ alignItems: "center" }}>
+
+              <div className='d-flex' style={{ alignItems: "center" }}>
+                <div className=''>
+                  <i class="ri-file-pdf-2-line fs-1"></i>
+                </div>
+                <div>
+                  Share as PDF
+
+                </div>
+
               </div>
             </div>
 
-            <div className="row mb-2">
-              <div className="col-5">
-                <span className="text-muted">Transaction No.</span>
-              </div>
-              <div className="col-7 text-end">
-                <span className="text-break text-muted" >250120020100886992707982</span>
-              </div>
-            </div>
 
-            <div className="row">
-              <div className="col-5">
-                <span className="text-muted">Session ID</span>
-              </div>
-              <div className="col-7 text-end">
-                <span className="text-break text-muted">100004250120135044125751241279</span>
-              </div>
-            </div>
-          </div>
-
-          {/* Footer Text */}
-          <div className="text-center text-muted small">
-            <p>Enjoy a better life with OPay. Get free transfers, withdrawals, bill payments, instant loans, and good annual interest on your savings. OPay is licensed by the Central Bank of Nigeria and insured by the NDIC.</p>
-          </div>
-
-          {/* Share Buttons */}
-          <div className="row g-2 mt-3">
-            <div className="col-6">
-              <button className="btn btn-outline-success w-100">
-                Share as image
-              </button>
-            </div>
-            <div className="col-6">
-              <button className="btn btn-outline-success w-100">
-                Share as PDF
-              </button>
-            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
+
   );
 }
 
