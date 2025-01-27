0154d93888332259629b89a86c6218e716a3c15a
devtescode <teslimagboola09@gmail.com>
2025-01-27 19:24:15 +0100

    update

--- utils/apiConfig.js
+++ utils/apiConfig.js
@@ -11,7 +11,7 @@ export const API_URLS = {
     gettransactions: (userId)=>`${baseURL}/useropay/gettransactions/${userId}`,
     getCounts: `${baseURL}/useropay/getCounts`,
     saveRecentTransaction: `${baseURL}/useropay/saveRecentTransaction`,
-    getrecentransaction: `${baseURL}/useropay/getrecentransaction`,
+    // getrecentransaction: `${baseURL}/useropay/getrecentransaction`,
     getrecentransaction: (userId, showAll)=>`${baseURL}/useropay/getrecentransaction/${userId}?showAll=${showAll}`,
     deleterecenttransaction: (transactionId) => `${baseURL}/useropay/deleterecenttransaction/${transactionId}`,
 };
