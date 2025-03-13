f6fc6b21eeba6dcf3da6526e4c87f0064942147e
devtescode <teslimagboola09@gmail.com>
2025-03-13 16:51:09 +0100

    update PWA

--- sw.js
+++ sw.js
@@ -9,31 +9,42 @@
 //  })
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    return caches.delete(cacheName); // Delete old caches
-                })
-            );
-        }).then(() => {
-            return caches.open('opay-cache').then(function(cache) {
-                return cache.addAll([
-                    '/opaydb',  // Cache the welcome page
-                ]);
-            });
+        caches.open('opay-cache').then(function(cache) {
+            return cache.addAll([
+                '/opaydb'  // Cache this first
+            ]);
         })
     );
-    self.skipWaiting(); // Force immediate activation
+    self.skipWaiting(); // Forces service worker to activate immediately
 });
 
 self.addEventListener('activate', function(event) {
-    event.waitUntil(self.clients.claim()); // Ensure the new SW takes control immediately
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== 'opay-cache') {
+                        return caches.delete(cacheName); // Delete old caches
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim(); // Ensures the new service worker takes control
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            caches.match('/opaydb').then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
