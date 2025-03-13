65cb991bceb27aa252997c47684acf00eab05d99
devtescode <teslimagboola09@gmail.com>
2025-03-13 15:00:30 +0100

    update PWA

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -6,33 +13,49 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'prompt', // Ensures the install prompt appears
+      registerType: 'autoUpdate',
       manifest: {
-        name: "Opay",
-        short_name: "Opay",
-        start_url: "/opaydb",
+        // name: 'Opay',
+        // start_url: "/opaydb", // Force the app to start at /opaydb
+        // short_name: 'OPay',
+        // description: 'My OPay Web App built with Vite',
+        // theme_color: '#ffffff',
+        // background_color: 'undefined',
+        start_url: "/opaydb", // Force the app to start at /opaydb
         display: "standalone",
-        theme_color: "#ffffff", 
+        background_color: "#00B875",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: '/icons/opay.jpg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/opay.jpg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
       workbox: {
         cleanupOutdatedCaches: true,
         sourcemap: true,
-      },
-      devOptions: {
-        enabled: true, // Allows PWA to work in development mode
-        type: 'module',
       },
     }),
   ],
   build: {
     rollupOptions: {
       output: {
+        // Custom naming for assets, chunks, and entry files
         assetFileNames: 'assets/[name].[ext]',
         chunkFileNames: 'assets/[name].[hash].js',
         entryFileNames: 'assets/[name].[hash].js',
+
+        // Separate vendor code into its own chunk
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor';
+            return 'vendor'; // This creates a vendor chunk for all dependencies
           }
         },
       },
