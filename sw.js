16b2257b4b38e2b6febff84fc5af3f4b4f2de52f
devtescode <teslimagboola09@gmail.com>
2025-03-13 16:39:51 +0100

    update PWA

--- sw.js
+++ sw.js
@@ -7,28 +7,25 @@
 //  self.addEventListener('install',function(event){
  
 //  })
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open('opay-cache').then(function(cache) {
+        caches.open("opay-cache").then(function (cache) {
             return cache.addAll([
-                '/opaydb',  // Cache the welcome page
-                '/'
+                "/opaydb",  // Force caching the welcome page
             ]);
         })
     );
     self.skipWaiting(); // Activate the service worker immediately
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(function (response) {
             if (response) {
-                return response; // Return cached response
+                return response;
             }
-            return fetch(event.request).catch(() => {
-                // If fetch fails (offline), serve opaydb as fallback
-                return caches.match('/opaydb');
-            });
+            return fetch(event.request).catch(() => caches.match("/opaydb"));
         })
     );
 });
+
