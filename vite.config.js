f1a690f0b6299ba08ce5963ad57cfcf74e04ce2e
devtescode <teslimagboola09@gmail.com>
2025-03-13 16:03:29 +0100

    update PWA

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -10,32 +16,26 @@ export default defineConfig({
       registerType: 'autoUpdate',
       manifest: {
         name: 'Opay',
-       
-        start_url: "/opaydb", 
-        display: "standalone",
-        background_color: "#00B875",
-        // theme_color: "#00B875",
+        short_name: 'OPay',
+        description: 'My OPay Web App built with Vite',
+        theme_color: '#ffffff',
+        background_color: '#00B875',
         icons: [
           {
             src: '/icons/opay.jpg',
             sizes: '192x192',
             type: 'image/png',
-            purpose: "any maskable",
           },
           {
             src: '/icons/opay.jpg',
             sizes: '512x512',
             type: 'image/png',
-            purpose: "any maskable",
           },
         ],
       },
       workbox: {
         cleanupOutdatedCaches: true,
         sourcemap: true,
-      },
-       devOptions: {
-        enabled: true, // ✅ Ensure PWA updates immediately in dev mode
       },
     }),
   ],
