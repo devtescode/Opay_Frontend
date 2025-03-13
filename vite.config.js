002d676c6b7395df60d1f8b09926af4ecfde3f6a
devtescode <teslimagboola09@gmail.com>
2025-03-13 14:43:29 +0100

    update PWA

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,20 @@ export default defineConfig({
       manifest: {
         start_url: "/opaydb", // Force the app to start at /opaydb
         display: "standalone",
-        display_override: ["minimal-ui"],  // Helps avoid splash screen
-     
+        background_color: "#00B875",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: '/icons/opay.jpg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/opay.jpg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
       workbox: {
         cleanupOutdatedCaches: true,
