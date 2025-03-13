16d101c79acb3a1b8ad37160d3328810a4f5598d
devtescode <teslimagboola09@gmail.com>
2025-03-13 14:10:50 +0100

    update PWA

--- src/assets/components/Opaypage.jsx
+++ src/assets/components/Opaypage.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Shield } from "lucide-react"
+import { useNavigate } from 'react-router-dom';
 
 const Opaypage = () => {
     const navigate = useNavigate();
