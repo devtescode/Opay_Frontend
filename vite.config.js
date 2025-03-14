76b5d244a8ddb85554ec5346596909240b0b89db
devtescode <teslimagboola09@gmail.com>
2025-03-14 15:47:43 +0100

    update admin page

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -7,16 +8,36 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      manifest: '/manifest.webmanifest', // Load from public/manifest.json
+      manifest: {
+        name: 'Opay',
+        short_name: 'OPay',
+        // start_url: '/opaydb',
+        description: 'My OPay Web App built with Vite',
+        theme_color: '#ffffff',
+        background_color: '#00B875',
+        icons: [
+          {
+            src: '/icons/opay.jpg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/opay.jpg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
       workbox: {
         cleanupOutdatedCaches: true,
-        navigateFallback: "/index.html",
+        sourcemap: true,
       },
     }),
   ],
   build: {
     rollupOptions: {
       output: {
+      
         assetFileNames: 'assets/[name].[ext]',
         chunkFileNames: 'assets/[name].[hash].js',
         entryFileNames: 'assets/[name].[hash].js',
@@ -24,7 +45,7 @@ export default defineConfig({
         // Separate vendor code into its own chunk
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // Creates a vendor chunk for dependencies
+            return 'vendor'; // This creates a vendor chunk for all dependencies
           }
         },
       },
