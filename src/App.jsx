1203dc583e30c0d50fd08c7d7ccb660e38f5f48c
devtescode <teslimagboola09@gmail.com>
2025-03-14 13:48:42 +0100

    update admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import UserLogin from './assets/components/UserLoginpage/UserLogin'
 import UserDb from './assets/components/UserDbpage/UserDb'
