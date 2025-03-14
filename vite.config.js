d3c58f31eb11323dc9feb0a73f5db3a1989c87dd
devtescode <teslimagboola09@gmail.com>
2025-03-14 15:16:47 +0100

    update admin page

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         start_url: '/',
         description: 'My OPay Web App built with Vite',
         theme_color: '#ffffff',
-        background_color: '#00B875',
+        background_color: '#ffffff',
         icons: [
           {
             src: '/icons/opay.jpg',
