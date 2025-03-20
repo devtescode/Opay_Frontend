0e1b5298dd82898deb271a8852008f902548d555
devtescode <teslimagboola09@gmail.com>
2025-03-20 15:10:08 +0100

    updated code for transaction page

--- src/assets/components/StoreTransactionpage/StoreTransaction.jsx
+++ src/assets/components/StoreTransactionpage/StoreTransaction.jsx
@@ -164,7 +164,7 @@ const StoreTransaction = ({ transactionStatus }) => {
                 )}
               </div>
               <div className="flex-grow-1">
-                <div className="text-truncate" style={{ fontSize: "12px" }}>Transfer to {transaction.accountName}</div>
+                <div className="text-truncate" style={{ fontSize: "12px",  whiteSpace: "normal", wordBreak: "break-word" }}>Transfer to {transaction.accountName}</div>
                 <small className="text-muted">
                   {/* {new Date(transaction.createdAt).toLocaleString()} */}
                   {format(new Date(transaction.createdAt), "MMM do, yyyy hh:mm:ss a")}
