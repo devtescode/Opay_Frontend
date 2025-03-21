7c2370e92aa33665f3d99c6fd5ddd58a12375f8a
devtescode <teslimagboola09@gmail.com>
2025-03-21 17:58:42 +0100

    updated the delect

--- src/assets/components/BalanceCard.jsx
+++ src/assets/components/BalanceCard.jsx
@@ -50,7 +50,7 @@ const BalanceCard = () => {
             </div>
             <div className="d-flex justify-content-between align-items-center">
                 <span className="fs-4 fw-bold d-flex" style={{ alignItems: "center" }}>
-                    {showBalance ? `₦${balance.toLocaleString()}.00` : "****"}
+                    {showBalance ? `₦${balance.toLocaleString()}.00` : "****"}<ChevronRight className='p-1'/>
                 </span>
                 <Button variant="light" className="text-success" onClick={AddMoneyBtn}>+ Add Money</Button>
             </div>
