152ae73898470998ee866a8abdf5ef2517a28e28
devtescode <teslimagboola09@gmail.com>
2025-06-30 16:17:38 +0100

    updated history page

--- src/assets/components/Takecontrol.jsx
+++ src/assets/components/Takecontrol.jsx
@@ -5,19 +5,19 @@ import message from '../../../public/Image/message.png'
 const Takecontrol = () => {
   return (
     <div className="mb-4 bg-white p-3">
-    <Card.Body className="d-flex align-items-center gap-3 border-none">
-      <div className="rounded-circle bg-light d-flex align-items-center justify-content-center" style={{ width: 40, height: 40 }}>
+    <Card.Body className="d-flex align-items-center gap-3 border-none justify-content-end mx-3">
+      {/* <div className="rounded-circle bg-light d-flex align-items-center justify-content-center" style={{ width: 40, height: 40 }}>
         <span className="fs-5" >
             <img src={message} alt="" />
         </span>
-      </div>
+      </div> */}
       <div>
-        <div className="fw-medium">Take Control, Stay infor...</div>
+        <div className="fw-medium">Take Control, Stay Informed</div>
         <div className="small text-muted">Add your email to get the  latest from Opay</div>
       </div>
-      <div>
+      {/* <div>
         <button className='btn btn-success px-3' >GO</button>
-      </div>
+      </div> */}
     </Card.Body>
   </div>
   )
