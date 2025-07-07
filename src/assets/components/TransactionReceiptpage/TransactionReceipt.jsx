c90c8c57d8b52c3207f78db8eefa6c0af0f4f5ef
devtescode <teslimagboola09@gmail.com>
2025-07-07 13:13:39 +0100

    update the receipt page

--- src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
+++ src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
@@ -166,139 +166,125 @@ function TransactionReceipt({ initialStatus }) {
         </p>
       </div>
       <div className="col-md-6 mx-auto col-sm-12 mt-2" style={{ maxWidth: "580px" }}>
-        {/* Header */}
-
-        {/* Receipt Card */}
-        <div className="card border-0">
-          <div className="card-body">
-            {/* Logo and Title */}
-            <div className="text-center mb-1 d-flex justify-content-between" style={{ alignItems: "center", marginTop:"-10px"}}>
-              <img
-                src="https://images.seeklogo.com/logo-png/50/1/opay-new-2023-logo-png_seeklogo-503616.png"
-                alt="Image description"
-                height="80"
-                // className="border border-dark"
-              />
-
-              <h5 className='text-muted'>Transaction Receipt</h5>
-            </div>
-
-            {/* Amount and Status */}
-            <div className="text-center mb-0">
-              <h2
-                className="mb-1"
-                style={{ color: "#01B575" }}
-              >
-                {amount !== null
-                  ? `₦${amount.toLocaleString(undefined, { minimumFractionDigits: 2 })}`
-                  : "Loading..."}
-              </h2>
-
-              <h5
-                onClick={handleDoubleTap}
-                className={`text-${status === "pending" ? "warning" : status === "failed" ? "danger" : "dark"} mb-0`}
-                style={{ fontWeight: "normal", cursor: "pointer" }}
-              >
-                {status
-                  ? status.charAt(0).toUpperCase() + status.slice(1)
-                  : "Loading..."}
-              </h5>
-
-              <small className="text-muted">
-                {passedTransaction
-                  ? formatDate(passedTransaction.createdAt) // Transaction time
-                  : formatDate() // Current time (fallback)
-                }
-              </small>
-            </div>
 
 
-            {/* {format(new Date(transaction.createdAt), "MMM do, yyyy hh:mm:ss a")} */}
-            {/* + "ful" */}
-            <hr />
+        <div className="receipt-container position-relative border border-danger">
+          <div className="card border-0">
+            <div className="watermark"></div>
+            <div className="card-body position-relative " style={{ zIndex: 1 }}>
 
-            {/* Transaction Details */}
-            <div className="mb-3">
-              <div className="row mb-2">
-                <div className="col-5">
-                  <span className="text-muted">Recipient Details</span>
-                </div>
-                <div className="col-7 text-end">
+              <div className="text-center mb-1 d-flex justify-content-between" style={{ alignItems: "center", marginTop: "-10px" }}>
+                <img
+                  src="https://images.seeklogo.com/logo-png/50/1/opay-new-2023-logo-png_seeklogo-503616.png"
+                  alt="Image description"
+                  height="80"
 
-                  {accountDetails ? (
-                    <div>
-                      <p className="mb-1 text-muted" style={{ fontSize: "12px" }}>{accountDetails.accountName}</p>
-                      <p className="mb-0 text-muted">
-                        <span>{accountDetails.bankName}</span> | <span
-                          style={{ userSelect: "text" }}
-                          onClick={() => {
-                            setTimeout(() => {
-                              window.getSelection()?.removeAllRanges(); // clears the highlight
-                            }, 5000); // wait a bit so user can copy
-                          }}
-                        >{accountDetails.accountNumber}</span>
-                      </p>
-                    </div>
-                  ) : (
-                    <p>No account details found. Please go back and select an account.</p>
-                  )}
-                </div>
+                />
+
+                <h5 className='text-muted'>Transaction Receipt</h5>
               </div>
 
-              <div className="row mb-2">
-                <div className="col-5">
-                  <span className="text-muted">Sender Details</span>
-                </div>
-                <div className="col-7 text-end">
+              <div className="text-center mb-0">
+                <h2
+                  className="mb-1"
+                  style={{ color: "#01B575" }}
+                >
+                  {amount !== null
+                    ? `₦${amount.toLocaleString(undefined, { minimumFractionDigits: 2 })}`
+                    : "Loading..."}
+                </h2>
 
-                  {/* {accountDetails ? ( */}
-                  <div>
+                <h5
+                  onClick={handleDoubleTap}
+                  className={`text-${status === "pending" ? "warning" : status === "failed" ? "danger" : "dark"} mb-0`}
+                  style={{ fontWeight: "normal", cursor: "pointer" }}
+                >
+                  {status
+                    ? status.charAt(0).toUpperCase() + status.slice(1)
+                    : "Loading..."}
+                </h5>
 
-                    <p className="mb-1 text-muted" style={{ fontSize: "12px", textTransform: "uppercase" }}>{userfullName ? userfullName : "No user data found"}</p>
-                    <p className="mb-0 text-muted">
-                      Opay |  {phoneNumber && ` ${phoneNumber.slice(0, 3)}****${phoneNumber.slice(7)}`}
-                    </p>
+                <small className="text-muted">
+                  {passedTransaction
+                    ? formatDate(passedTransaction.createdAt)
+                    : formatDate()
+                  }
+                </small>
+              </div>
 
-                    {/* {userfullName ? userfullName : "No user data found"} */}
+
+
+              <div className="mb-3">
+                <div className="row mb-2">
+                  <div className="col-5">
+                    <span className="text-muted">Recipient Details</span>
                   </div>
-                  {/* ) : (
-                    <p>No account details found. Please go back and select an account.</p>
-                  )} */}
-                </div>
-              </div>
-              {/* <div className="row mb-2">
-                <div className="col-5">
-                  <span className="text-muted">Transaction Type</span>
-                </div>
-                <div className="col-7 text-end">
-                  <span className="text-muted">Transfer to Bank Account</span>
-                </div>
-              </div> */}
+                  <div className="col-7 text-end">
 
-              <div className="row mb-2">
-                <div className="col-5">
-                  <span className="text-muted">Transaction No.</span>
+                    {accountDetails ? (
+                      <div>
+                        <p className="mb-1 text-muted" style={{ fontSize: "12px" }}>{accountDetails.accountName}</p>
+                        <p className="mb-0 text-muted">
+                          <span>{accountDetails.bankName}</span> | <span
+                            style={{ userSelect: "text" }}
+                            onClick={() => {
+                              setTimeout(() => {
+                                window.getSelection()?.removeAllRanges();
+                              }, 5000);
+                            }}
+                          >{accountDetails.accountNumber}</span>
+                        </p>
+                      </div>
+                    ) : (
+                      <p>No account details found. Please go back and select an account.</p>
+                    )}
+                  </div>
                 </div>
-                <div className="col-7 text-end">
-                  <span className="text-break text-muted" >{transactionId}</span>
+
+                <div className="row mb-2">
+                  <div className="col-5">
+                    <span className="text-muted">Sender Details</span>
+                  </div>
+                  <div className="col-7 text-end">
+
+
+                    <div>
+
+                      <p className="mb-1 text-muted" style={{ fontSize: "12px", textTransform: "uppercase" }}>{userfullName ? userfullName : "No user data found"}</p>
+                      <p className="mb-0 text-muted">
+                        Opay |  {phoneNumber && ` ${phoneNumber.slice(0, 3)}****${phoneNumber.slice(7)}`}
+                      </p>
+
+                    </div>
+
+                  </div>
+                </div>
+
+                <div className="row mb-2">
+                  <div className="col-5">
+                    <span className="text-muted">Transaction No.</span>
+                  </div>
+                  <div className="col-7 text-end">
+                    <span className="text-break text-muted" >{transactionId}</span>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-5">
+                    <span className="text-muted">Session ID</span>
+                  </div>
+                  <div className="col-7 text-end">
+                    <span className="text-break text-muted">100004250120135044125751241279</span>
+                  </div>
                 </div>
               </div>
 
-              <div className="row">
-                <div className="col-5">
-                  <span className="text-muted">Session ID</span>
-                </div>
-                <div className="col-7 text-end">
-                  <span className="text-break text-muted">100004250120135044125751241279</span>
-                </div>
+              <div className="text-center text-muted small " style={{ fontSize: "9px", marginTop: "50px" }}>
+                <p style={{ textAlign: "justify" }}>Enjoy a better life with OPay. Get free transfers, withdrawals, bill payments, instant loans, and good annual interest on your savings. OPay is licensed by the Central Bank of Nigeria and insured by the NDIC.</p>
               </div>
+
+
             </div>
-
-            <div className="text-center text-muted small " style={{ fontSize: "9px", marginTop: "50px" }}>
-              <p style={{ textAlign: "justify" }}>Enjoy a better life with OPay. Get free transfers, withdrawals, bill payments, instant loans, and good annual interest on your savings. OPay is licensed by the Central Bank of Nigeria and insured by the NDIC.</p>
-            </div>
-
-
           </div>
         </div>
         <div className="row g-2 mt-3 fixed-bottom shadow-lg " style={{ alignItems: "center" }}>
@@ -323,9 +309,7 @@ function TransactionReceipt({ initialStatus }) {
                 <div className='text-muted'>
                   |
                 </div>
-                {/* <div>
-                  |
-                </div> */}
+
               </div>
             </div>
 
