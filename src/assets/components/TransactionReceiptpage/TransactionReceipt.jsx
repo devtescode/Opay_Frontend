9c1c123c69b0659a98503299bc620c63e3549827
devtescode <teslimagboola09@gmail.com>
2025-07-07 19:00:52 +0100

    update frontend

--- src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
+++ src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
@@ -347,10 +347,9 @@ function TransactionReceipt({ initialStatus }) {
                     </div>
                   </div>
 
-                  <div className="text-center text-muted small " style={{ fontSize: "9px", marginTop: "50px" }}>
+                  <div className="text-center text-muted small " style={{ fontSize: "9px", marginTop: "30px" }}>
                     <p style={{ textAlign: "justify" }}>Enjoy a better life with OPay. Get free transfers, withdrawals, bill payments, instant loans, and good annual interest on your savings. OPay is licensed by the Central Bank of Nigeria and insured by the NDIC.</p>
                   </div>
-                  {/* Wavy Bottom Edge */}
 
 
                 </div>
