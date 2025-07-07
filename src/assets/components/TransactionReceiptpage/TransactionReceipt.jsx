88b45c6eac78866802652ca8ce7cea4015677ae5
devtescode <teslimagboola09@gmail.com>
2025-07-07 17:13:52 +0100

    update frontend

--- src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
+++ src/assets/components/TransactionReceiptpage/TransactionReceipt.jsx
@@ -215,7 +215,7 @@ function TransactionReceipt({ initialStatus }) {
                   style={{
                     fontSize: '30px',
                     color: '#000',
-                    opacity: 0.02,
+                    opacity: 0.07,
                     fontWeight: 'bold'
                   }}
                 >
